df9861c0f43cd86ecc13dec2663bcae6be035622
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2022-10-20 11:32:27 +0200

    fix: ucfirst locale not required

--- src/helpers/ucfirst.ts
+++ src/helpers/ucfirst.ts
@@ -1,7 +1,7 @@
 /**
  * Capitalize first letter in string
  */
-const ucfirst = (str: string, locale: string): string => {
+const ucfirst = (str: string, locale?: string): string => {
   const [first, ...rest] = str;
 
   return first.toLocaleUpperCase(locale) + rest.join('');
