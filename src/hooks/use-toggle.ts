4562c4ed7163a24d58d4ec9d1a90596e99ed7d12
AlexDanilovich <alexeidanilovich031193@gmail.com>
2022-11-24 17:56:07 +0300

    refactor: changed useToggle return

--- src/hooks/use-toggle.ts
+++ src/hooks/use-toggle.ts
@@ -2,16 +2,22 @@ import { useCallback, useMemo, useState } from 'react';
 
 /**
  * Switch between true or false
- * [isToggled, toggle, close]
  */
-const useToggle = (initialState = false): [boolean, () => void, () => void] => {
-  const [isToggled, setIsToggled] = useState(initialState);
+const useToggle = (
+  initialState = false,
+): {
+  isToggled: boolean;
+  toggle: () => void;
+  close: () => void;
+  setToggled: (value: boolean) => void;
+} => {
+  const [isToggled, setToggled] = useState(initialState);
 
-  const toggle = useCallback(() => setIsToggled((prevIsToggled) => !prevIsToggled), []);
+  const toggle = useCallback(() => setToggled((prevIsToggled) => !prevIsToggled), []);
 
-  const close = useCallback(() => setIsToggled(false), []);
+  const close = useCallback(() => setToggled(false), []);
 
-  return useMemo(() => [isToggled, toggle, close], [close, isToggled, toggle]);
+  return useMemo(() => ({ isToggled, toggle, close, setToggled }), [close, isToggled, toggle]);
 };
 
 export default useToggle;
