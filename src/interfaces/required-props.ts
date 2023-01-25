f14a9c7cd3bb780f7d330daaad84bf5afa72d1d4
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-01-25 11:38:34 +0100

    feat: add required only type

--- src/interfaces/required-props.ts
+++ src/interfaces/required-props.ts
@@ -1,1 +1,3 @@
 export type RequiredProps<T, TKeys extends keyof T> = Partial<T> & Required<Pick<T, TKeys>>;
+
+export type RequiredOnlyProps<T, TKeys extends keyof T> = T & Required<Pick<T, TKeys>>;
