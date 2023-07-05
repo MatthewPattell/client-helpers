76bbcb28c7c8bcc5d2d03f00dd5c79e47f3736c2
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-07-05 23:45:24 +0200

    fix: fc with children interface generic default type

--- src/interfaces/fc-with-children.ts
+++ src/interfaces/fc-with-children.ts
@@ -3,4 +3,4 @@ import type { FC, PropsWithChildren } from 'react';
 /**
  * Custom Type for a React functional component with props AND CHILDREN
  */
-export type FCC<TP extends object = any> = FC<PropsWithChildren<TP>>;
+export type FCC<TP = Record<string, any>> = FC<PropsWithChildren<TP>>;
