f422ff2464950f2d21123dde167590cde30d7c85
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-11-06 10:40:29 +0100

    fix: yup number stripEmptyString, add replaceComma method for number validation

--- src/helpers/extend-yup.ts
+++ src/helpers/extend-yup.ts
@@ -9,7 +9,12 @@ function ExtendYupValidation(): void {
     return this.transform((value: string) => (value === '' ? null : value));
   });
   addMethod(number, 'stripEmptyString', function () {
-    return this.transform((__, value: string) => (value === '' ? null : value));
+    return this.transform((__, value: string) => (value === '' ? null : Number(value)));
+  });
+  addMethod(number, 'replaceComma', function () {
+    return this.transform((__, val: string | number) =>
+      typeof val === 'string' ? Number(val.replace(/,/, '.')) : val,
+    );
   });
 }
 
